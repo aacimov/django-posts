1088ed4c64e89800f4b47fe5204880c38dd2acd0
Aleks Acimovic <aleks@beerent.com>
2019-07-03 14:04:32 +0200

    Added initial migration

--- build/lib/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from posts.views import PostDetail, PostList
-
-urlpatterns = [
-    url(r'^(?P<id>[\w-]+)/(?P<slug>[\w-]+)/', PostDetail.as_view(), name='post_detail'),
-    url(r'^', PostList.as_view(), name='posts_list'),
-]
---
+++ posts/migrations/0001_initial.py
@@ -0,0 +1,59 @@
+import autoslug.fields
+import cms.models.fields
+from django.db import migrations, models
+import django.db.models.deletion
+import django.utils.timezone
+import model_utils.fields
+import posts.models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
+                ('title', models.CharField(max_length=255, verbose_name='Title')),
+                ('title_hr', models.CharField(max_length=255, null=True, verbose_name='Title')),
+                ('title_en', models.CharField(max_length=255, null=True, verbose_name='Title')),
+                ('slug', autoslug.fields.AutoSlugField(always_update=True, editable=False, populate_from='title', unique=True, verbose_name='Slug')),
+                ('detail_url', models.CharField(blank=True, max_length=255, null=True, verbose_name='Detail url')),
+                ('published', models.BooleanField(blank=True, default=False, verbose_name='Published')),
+                ('post_content', cms.models.fields.PlaceholderField(editable=False, null=True, on_delete=django.db.models.deletion.CASCADE, slotname=testproject.app.django-posts.posts.models.post_detail_placeholder, to='cms.Placeholder')),
+            ],
+            options={
+                'verbose_name': 'Post',
+                'verbose_name_plural': 'Posts',
+            },
+        ),
+        migrations.CreateModel(
+            name='Section',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
+                ('title', models.CharField(max_length=255, verbose_name='Title')),
+                ('title_hr', models.CharField(max_length=255, null=True, verbose_name='Title')),
+                ('title_en', models.CharField(max_length=255, null=True, verbose_name='Title')),
+                ('slug', autoslug.fields.AutoSlugField(always_update=True, editable=False, populate_from='title', unique=True)),
+                ('namespace', models.CharField(max_length=255, verbose_name='Namespace')),
+            ],
+            options={
+                'verbose_name': 'Section',
+                'verbose_name_plural': 'Sections',
+            },
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='section',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='post_section', to='posts.Section', verbose_name='Section'),
+        ),
+    ]
